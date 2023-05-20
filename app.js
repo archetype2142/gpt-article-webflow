955b139d760f6b925c7524da10694b6930ef4df7
Ritwick Malhotra <malhotraritwick2011@gmail.com>
2023-05-21 01:42:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config({ path: __dirname + '/.env' })
+const Webflow = require('webflow-api');
+const fs = require('fs');
+const path = require('path')
+
+const webflow = new Webflow({
+  token: process.env.WEBFLOW_TOKEN,
+  version: "1.0.0",
+  headers: {
+    "User-Agent": "My Webflow App / 1.0",
+  },
+});
+
+const files = fs.readdirSync('./articles/');
+
+files.forEach(fileName => {
+  fs.readFile(`./articles/${fileName}`, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    // uploadArticle(JSON.parse(data).article)
+
+    console.log(JSON.parse(data).article);
+  });
+})
+
+async function uploadArticle(article) {
+
+  const created_item = await webflow.createItem({
+    collectionId: process.env.COLLECTION_ID,
+    fields: { 
+      slug: article.slug,
+      name: article.name,
+      _archived: false,
+      _draft: false,
+      'post-body': article.body
+    }
+  })
+
+  console.log(created_item)
+}
